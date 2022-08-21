5808124641fa6aaa2cb696f3bc45dffb7408289c
js2102 <ssen199504@gmail.com>
2022-08-21 15:16:52 +0900

    Add Baseline component

---
+++ src/components/Common/Baseline.jsx
@@ -0,0 +1,14 @@
+export const Baseline = ({ children }) => (
+  <div
+    style={{
+      margin: "24px 0",
+    }}>
+    <hr
+      style={{
+        borderTop: "2px solid #646fd4",
+        padding: "24px 0",
+      }}>
+      {children}
+    </hr>
+  </div>
+);
