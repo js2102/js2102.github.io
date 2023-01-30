8feb136191c72d82d12dab34590b250ed2d8c7ba
js2102 <ssen199504@gmail.com>
2023-01-30 11:19:15 +0900

    Modify post list wrapper mobile width

--- src/components/Home/PostList.jsx
+++ src/components/Home/PostList.jsx
@@ -11,7 +11,7 @@ export const Wrapper = ({ children }) => {
     margin: "64px auto",
 
     "@media only screen and (max-width: 768px)": {
-      width: "70%",
+      width: "80%",
       minWidth: 0,
     },
   });
